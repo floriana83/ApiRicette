6191865aef5e57211be9cda91c05067767e10925
FL0 <92723694+floriana83@users.noreply.github.com>
2022-04-22 18:49:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
-// import cors from 'cors';
+import cors from 'cors';
 import ricetteRoutes from './routes/ricette.js'
 const app = express();
 import dotenv from 'dotenv';
-const cors = require('cors')
+
 
 app.use(express.json())
 dotenv.config()
 const PORT = process.env.PORT || 3000;
-app.use('/ricette', ricetteRoutes )
 app.use(cors())
-app.options('*',cors())
+app.options("*",cors())
 
+app.use('/ricette', ricetteRoutes )
 app.get('/', (req, res) => res.send("benvenuto nella homepage"))
 
 
